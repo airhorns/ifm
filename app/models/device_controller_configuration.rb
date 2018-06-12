5aabd44a0b9c592a3b17b928cde737d00dd5d04f
Harry Brundage <harry.brundage@gmail.com>
2018-06-12 16:31:50 -0400

    Make tests pass

--- app/models/device_controller_configuration.rb
+++ app/models/device_controller_configuration.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviceControllerConfiguration < ApplicationRecord
-  validates :human_name, presence: true
+  validates :nickname, presence: true
   belongs_to :farm
   belongs_to :device_configuration
 end
