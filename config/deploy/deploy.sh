4662d484d19805e6aed66aa05b9991d2056de849
Harry Brundage <harry.brundage@gmail.com>
2018-06-13 13:51:47 -0400

    Kubernetes cloudsql proxy + secrets

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -5,4 +5,5 @@ export REVISION=${CIRCLE_SHA1}
 export KUBECONFIG=${HOME}/.kube/config
 export ENVIRONMENT=production
 
-bundle exec kubernetes-deploy --template-dir=config/deploy/$ENVIRONMENT ifm-production gke_integrated-farm-manager_us-central1-a_development-tiny
+# kubernetes-deploy comes from the gem installed in the docker container this runs in in circle.
+kubernetes-deploy --template-dir=config/deploy/$ENVIRONMENT ifm-production gke_integrated-farm-manager_us-central1-a_development-tiny
