910092edfd6077b151fbe6f089db134ecb735576
Harry Brundage <harry.brundage@gmail.com>
2018-06-13 19:46:38 -0400

    Assets in production?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['RAILS_ASSET_HOST'] || "/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
