bca20e7937bef75ad1d74428493cf76ebc776516
Harry Brundage <harry.brundage@gmail.com>
2018-06-13 17:08:28 -0400

    OMG IT WORKS

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # This script is run by the CD platform to deploy the app.
 set -ex
-export REVISION=${CIRCLE_SHA1}
 export KUBECONFIG=${HOME}/.kube/config
 export ENVIRONMENT=production
 
-# kubernetes-deploy comes from the gem installed in the docker container this runs in in circle.
-kubernetes-deploy --template-dir=config/deploy/$ENVIRONMENT ifm-production gke_integrated-farm-manager_us-central1-a_development-tiny
+if [[ -z "${CIRCLECI}" ]]; then
+  # kubernetes-deploy is in the bundle
+  export REVISION=$(git rev-parse HEAD)
+  DEPLOY_COMMAND="bundle exec kubernetes-deploy"
+else
+  # kubernetes-deploy comes from the gem installed in the docker container this runs in in circle.
+  DEPLOY_COMMAND="kubernetes-deploy"
+  export REVISION=${CIRCLE_SHA1}
+fi
+
+eval "$DEPLOY_COMMAND --template-dir=config/deploy/$ENVIRONMENT ifm-production gke_integrated-farm-manager_us-central1-a_development-tiny"
