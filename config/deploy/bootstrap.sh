59edd6f0bcca029c29ba95e7f3f0c3b8bb38344e
Harry Brundage <harry.brundage@gmail.com>
2018-10-29 16:03:03 -0400

    Migrate to berries.fun

--- config/deploy/bootstrap.sh
+++ config/deploy/bootstrap.sh
@@ -3,14 +3,15 @@
 # Note: the user running this has to have cluster admin priviledges!
 # Run kubectl create clusterrolebinding harry-cluster-admin-binding --clusterrole=cluster-admin --user=harry.brundage@gmail.com for example to give yourself priviledges on a new cluster
 set -ex
-kubectl create -f config/deploy/tiller-service-account.yaml
+kubectl create -f config/deploy/cluster-helm/tiller-service-account.yaml
 helm init --service-account tiller
 helm repo update
 kubectl create namespace ifm-production
 
 sleep 30
 helm install --name cert-manager --namespace kube-system stable/cert-manager
-helm install --name nginx-ingress stable/nginx-ingress -f config/deploy/nginx-ingress-controller.yml
+helm install --name nginx-ingress stable/nginx-ingress -f config/deploy/cluster-helm/nginx-ingress-controller.yml
+helm install --name external-dns stable/external-dns -f config/deploy/cluster-helm/external-dns.yaml
 
 helm repo add influx http://influx-charts.storage.googleapis.com
-helm install --name telegraf influx/telegraf-ds -f config/deploy/telegraf.yaml
+helm install --name telegraf influx/telegraf-ds -f config/deploy/cluster-helm/telegraf.yaml
